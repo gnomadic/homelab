90ee976ce764da7b858c377f93e8e9466ce3e1e2
gnomadic <edflem+github@gmail.com>
2025-06-02 14:01:25 -0400

    flip high low

--- controller/buttons.py
+++ controller/buttons.py
@@ -5,9 +5,9 @@ GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button 1
 GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button 2
 
 while True:
-    if GPIO.input(37) == GPIO.LOW:
+    if GPIO.input(37) == GPIO.HIGH:
         print("Button 1 pressed")
         # Add your button 1 action here
-    if GPIO.input(36) == GPIO.LOW:
+    if GPIO.input(36) == GPIO.HIGH:
         print("Button 2 pressed")
         # Add your button 2 action here
