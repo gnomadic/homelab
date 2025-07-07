6d29bf6b18579f8e751c89ff8b80c1520953368f
gnomadic <edflem+github@gmail.com>
2025-07-07 11:20:11 -0400

    ok

--- controller/buttons2.py
+++ controller/buttons2.py
@@ -21,6 +21,8 @@ GPIO.setup(BUTTON3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # Lock state to prevent bouncing/repeat
 button1_locked = False
 button2_locked = False
+button3_locked = False
+
 
 def unlock_button1():
     global button1_locked
@@ -37,28 +39,28 @@ def unlock_button3():
 def button1_callback(channel):
     global button1_locked
     if not button1_locked:
-        button1_locked = True
+        # button1_locked = True
         print("Button 1 pressed")
         # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         # script_path = os.path.join(BASE_DIR, "scripts/redraw.sh")
         # subprocess.run([script_path])
-        threading.Timer(5.0, unlock_button1).start()
+        # threading.Timer(5.0, unlock_button1).start()
 
 def button2_callback(channel):
     global button2_locked
     if not button2_locked:
-        button2_locked = True
+        # button2_locked = True
         print("Button 2 pressed")
         # Your button 2 action here
-        threading.Timer(5.0, unlock_button2).start()
+        # threading.Timer(5.0, unlock_button2).start()
 
 def button3_callback(channel):
     global button3_locked
     if not button3_locked:
-        button3_locked = True
+        # button3_locked = True
         print("Button 3 pressed")
         # Your button 3 action here
-        threading.Timer(5.0, unlock_button3).start()
+        # threading.Timer(5.0, unlock_button3).start()
 
 # Register callbacks
 GPIO.add_event_detect(BUTTON1_PIN, GPIO.FALLING, callback=button1_callback, bouncetime=200)
