122f991a81ab80a72b3eb91a6f528e51622ae4a7
gnomadic <edflem+github@gmail.com>
2025-07-07 11:14:26 -0400

    3 buttons

--- controller/buttons2.py
+++ controller/buttons2.py
@@ -6,9 +6,13 @@ import os
 
 GPIO.setmode(GPIO.BOARD)
 
+# modded GPIO 21 (pin 16) to be busy on inkywhat
+
 # Use PUD_UP because Cherry MX connects to GND when pressed
 BUTTON1_PIN = 37
 BUTTON2_PIN = 36
+BUTTON3_PIN = 31
+# BUTTON3_PIN
 
 GPIO.setup(BUTTON1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTON2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -25,14 +29,18 @@ def unlock_button2():
     global button2_locked
     button2_locked = False
 
+def unlock_button3():
+    global button3_locked
+    button3_locked = False
+
 def button1_callback(channel):
     global button1_locked
     if not button1_locked:
         button1_locked = True
         print("Button 1 pressed")
-        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        script_path = os.path.join(BASE_DIR, "scripts/redraw.sh")
-        subprocess.run([script_path])
+        # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        # script_path = os.path.join(BASE_DIR, "scripts/redraw.sh")
+        # subprocess.run([script_path])
         threading.Timer(5.0, unlock_button1).start()
 
 def button2_callback(channel):
@@ -43,9 +51,18 @@ def button2_callback(channel):
         # Your button 2 action here
         threading.Timer(5.0, unlock_button2).start()
 
+def button3_callback(channel):
+    global button3_locked
+    if not button3_locked:
+        button3_locked = True
+        print("Button 3 pressed")
+        # Your button 3 action here
+        threading.Timer(5.0, unlock_button3).start()
+
 # Register callbacks
 GPIO.add_event_detect(BUTTON1_PIN, GPIO.FALLING, callback=button1_callback, bouncetime=200)
 GPIO.add_event_detect(BUTTON2_PIN, GPIO.FALLING, callback=button2_callback, bouncetime=200)
+GPIO.add_event_detect(BUTTON3_PIN, GPIO.FALLING, callback=button2_callback, bouncetime=200)
 
 try:
     print("Listening for button presses...")
